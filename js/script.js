6617610a146461fa879601f3e7661bf42374c5fc
NastyaGapich <example@gmail.com>
2022-07-26 22:26:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,105 @@
+"use strict";
+
+
+     //snake_case
+     //UPPER_SNAKE_CASE
+     //Kebub-case
+     //PascalCase
+
+     //simple data type
+// let number = 4.6;
+// console.log(-4/0);
+// console.log("string" * 9);
+
+// const person = "Alex";
+
+// const bool = true;    //console.log(something); //null
+
+// let und;
+// console.log(und);
+
+   //object
+   //внутри описание этого объекта
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried:false,
+// };
+   //console.log(obj.age);
+// console.log(obj["age"]);
+
+//             0          1             2   3            4  5
+// let arr = ['plum.png', 'orange.jpg', 7 ,'apple.bmp', {},[]]; //массив
+// console.log(arr[1]);
+
+//разница между объектами и массивами и неочивидные синтаксические возможности
+
+// const arr = ['a','b','c'];
+// arr[10] = '1234';
+
+// console.log(arr);
+
+// const arrObj = {
+//     a:'a',
+//     '1':'b',
+//     2:'c',
+//     abc: {
+       //объект в объекте 
+//       df:[{},{}]
+//       def: {
+
+//       }
+//     }
+// };
+
+   //arrObj.b = '1234';
+// arrObj['b']= [];
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj = {
+//   Anna:500,
+//   Alice:700
+// };
+
+
+//alert('Hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = +prompt('Do you have 18?','18');
+// console.log(answer + 5);
+
+//const answers = [];
+
+// answers[0] = prompt('what`s your name?', '');
+// answers[1] = prompt('what`s your lost name?', '');
+// answers[2] = prompt('how old you?', '');
+
+//console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Nastya';
+// alert(`hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(50%3);
+
+console.log(2*4 === 8);
+
+const isChect = false,
+      isClose = true;
+
+console.log(isChect|| !isClose);    //false  
